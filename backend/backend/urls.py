03d64d54c47d5c200f31a2ce0b713aaafc9ab389
Elena Baranova <guestagain@yandex.ru>
2023-11-03 14:00:45 +0300

    fixed flake8

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,23 +22,23 @@ urlpatterns = [
 
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="OSDC API",
-      default_version='v1',
-      description="Документация для проекта OSDC",
-      # terms_of_service="URL страницы с пользовательским соглашением",
-      contact=openapi.Contact(email="admin@admin.ru"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="OSDC API",
+        default_version='v1',
+        description="Документация для проекта OSDC",
+        # terms_of_service="URL страницы с пользовательским соглашением",
+        contact=openapi.Contact(email="admin@admin.ru"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns += [
-   url(r'^swagger(?P<format>\.json|\.yaml)$', 
-       schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), 
-       name='schema-swagger-ui'),
-   url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), 
-       name='schema-redoc'),
-] 
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
+]
