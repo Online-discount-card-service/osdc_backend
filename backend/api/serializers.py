5d97e070367255f94c4c873543356dc2205387c7
Victoria Titova <krilatixnet@yandex.ru>
2023-11-16 14:38:06 +0300

    Вернула сериализатор магазинов к первоначальному рабочему варианту

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,7 +5,6 @@ from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 
 from core.models import Card, Group, Shop
-
 from users.models import User
 
 
@@ -36,19 +35,18 @@ class ShopSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Магазины."""
 
     group = GroupSerializer(many=True)
-    image = Base64ImageField(read_only=True) 
 
     class Meta:
         model = Shop
         fields = '__all__'
 
 
-class ShopCreateSerializer(serializers.ModelSerializer):
-    """Сериализатор для создания Магазина."""
+# class ShopCreateSerializer(serializers.ModelSerializer):
+    # """Сериализатор для создания Магазина."""
 
-    class Meta:
-        model = Shop
-        fields = 'name'
+    # class Meta:
+        # model = Shop
+        # fields = 'name'
 
 
 class CardSerializer(serializers.ModelSerializer):
