c09ab77b27e4a42a982285ab12f45bde55dbb807
Maxim Solodilov <sldmxm@gmail.com>
2023-11-18 01:08:41 +0200

    CORS для локальной разработки фронта

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,11 +33,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'sorl.thumbnail',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
-    # 'django_filters',
     'api',
     'core',
     'users.apps.UsersConfig',
@@ -45,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -147,3 +147,10 @@ DJOSER = {
     },
     'HIDE_USERS': False,
 }
+
+# TODO убрать на продакшене
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]
