338180cae6004f73055206c52bef4121b4ca2f23
Maxim Solodilov <sldmxm@gmail.com>
2023-11-18 02:18:47 +0200

    CORS только localhost

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -149,8 +149,8 @@ DJOSER = {
 }
 
 # TODO убрать на продакшене
-CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:5173',
-#     'http://127.0.0.1:5173',
-# ]
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]
