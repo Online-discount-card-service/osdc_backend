63bb93805ef455ed9c3eeda3ccbfd2ece679e466
Krasin Anton <kr.toxa37@yandex.ru>
2023-11-19 19:05:41 +0300

    удалил password из models users

--- backend/users/models.py
+++ backend/users/models.py
@@ -55,9 +55,6 @@ class User(AbstractUser):
         blank=False,
         unique=True
     )
-    password = models.TextField(
-        verbose_name=_('Пароль'),
-    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
