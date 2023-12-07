55afb78ee6fd2fb8f384c3fbd6fe34132bc1e553
Victoria Titova <krilatixnet@yandex.ru>
2023-12-07 22:09:13 +0300

    Flake8 invisible mistakes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,13 +96,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 AUTH_USER_MODEL = 'users.User'
 AUTH_PASSWORD_VALIDATORS = [
     # {
-        # 'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     # },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     # {
-        # 'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     # },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
